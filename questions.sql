615e5b784900bbce9203a5699d504fe06e28155b
jpking872 <34683254+jpking872@users.noreply.github.com>
2019-06-06 15:59:46 -0400

    Add files via upload

---
+++ questions.sql
@@ -0,0 +1,92 @@
+-- Here are some questions to answer
+-- Keep the questions in the file, and just put the answers below the questions.
+
+/*
+  About the DATA
+  There are 4 tables
+  here is a list with descriptions
+
+  IMPORTANT: YOU MAY CHANGE THE TABLE STRUCTURES IF YOU WOULD LIKE.
+      THE LAST QUESTION WILL ASK ABOUT ALL YOUR CHANGES.
+
+  - users
+     - just a list of user data
+  - emails
+     - holds users emails.
+     - There is a one to many relationship with the users table. Each user can have many emails
+     - One email is marked as the primary email for the user
+  - usage_log
+     - holds the users session dates and times.
+     - contains the login and logout times of every user session.
+     - So every time a user logs in, it creates a new entry in this table
+  - users_admin
+     - only holds a user id
+     - if a user's id is in this table, then they are an admin
+*/
+
+-- EXAMPLE
+-- Write a statement that will return all the users
+--  with the last name 'Johnson'
+SELECT *
+  FROM users
+  WHERE lName = 'Johnson';
+
+
+-- QUESTION 1
+-- write a statement that returns all the users data
+--   including their primary email, if they have one
+--   and if they are an admin or not
+
+SELECT * FROM users u LEFT JOIN emails e ON u.id = e.userid;
+
+-- QUESTION 2
+-- write a statement that returns all user data
+--   including their primary email
+--   and if they are an admin or not
+--   but only users with emails
+
+SELECT * FROM users u INNER JOIN email e ON u.id = e.userid;
+
+
+-- QUESTION 3
+-- write a statement that returns all user data
+--   that do not have an email
+--   and are not admins
+
+
+-- QUESTION 4
+-- write a statement that returns all the users data
+--    only users with last name that contains a letter 'B'
+--    and also return the number of emails those users have
+
+SELECT * FROM users u LEFT JOIN (SELECT e.userid, COUNT(*) FROM email e) as numemails ON u.id = e.userid WHERE u.lName LIKE '%B%'
+
+-- QUESTION 5
+-- write a statement that returns all the users data
+--    only users that have more than one email
+--    and are admins
+
+
+-- QUESTION 6
+-- write a statement that returns all user data
+--   with the total amount of time the users have spent on the site
+--   in the past 21 days, in minutes
+
+
+
+-- QUESTION 7
+-- Write a statement that returns all user data
+--   with the total amount of time spent on the site
+--   and with the total number of logins
+--   beginning of time
+
+
+-- QUESTION 8
+-- given the table structure provided.
+-- How would you did/would you change/improve our schema? Any Why?
+-- Please list all changes that were made and a justification for the change.
+
+I would add a userid column in the email table as a foreign key to the user table.
+I would add a userid column in the usage_log table as a foreign key to the user table.
+
+They are necessary to complete the queries.
